bc3c9835508313638933442f2ded878d4e194a3b
moifire <80068338+moifire@users.noreply.github.com>
2026-04-11 00:19:41 +0200

    Add files via upload

--- api/manifest.js
+++ api/manifest.js
@@ -1,22 +1,10 @@
+const {requirePrivateAccess}=require('./lib/private');
+const {sendJson,handleOptions}=require('./lib/common');
 
-const { sendJson, handleOptions, requireAddonToken, buildCatalogsFromDb } = require('./lib/common');
-const { getCatalog } = require('./lib/db');
+module.exports=async(req,res)=>{
+ if(handleOptions(req,res))return;
+ const user=await requirePrivateAccess(req,res);
+ if(!user)return;
 
-module.exports = async (req, res) => {
-  if (handleOptions(req, res)) return;
-  if (!requireAddonToken(req, res)) return;
-
-  const db = await getCatalog();
-  const manifest = {
-    id: 'com.moitube.ultra.private.db',
-    version: '3.0.0',
-    name: db.addonsBrand?.name || 'MoiTube Ultra Private Legal PRO',
-    description: db.addonsBrand?.description || 'Addon privado con base de datos',
-    logo: db.addonsBrand?.logo || '',
-    resources: ['catalog', 'meta', 'stream'],
-    types: ['movie', 'series', 'tv'],
-    catalogs: buildCatalogsFromDb(db),
-    behaviorHints: { configurable: false, configurationRequired: false }
-  };
-  return sendJson(res, 200, manifest);
+ sendJson(res,200,{id:"addon",name:"PRIVATE",version:"1.0.0"});
 };
--- api/meta.js
+++ api/meta.js
@@ -1,15 +1,10 @@
-const { sendJson, handleOptions, requireAddonToken, makeMeta, parseUrl } = require('./lib/common');
-const { getCatalog } = require('./lib/db');
+const {requirePrivateAccess}=require('./lib/private');
+const {sendJson,handleOptions}=require('./lib/common');
 
-module.exports = async (req, res) => {
-  if (handleOptions(req, res)) return;
-  if (!requireAddonToken(req, res)) return;
+module.exports=async(req,res)=>{
+ if(handleOptions(req,res))return;
+ const user=await requirePrivateAccess(req,res);
+ if(!user)return;
 
-  const url = parseUrl(req);
-  const type = url.searchParams.get('type');
-  const id = url.searchParams.get('id');
-  const db = await getCatalog();
-  const item = db.items.find(x => x.id === id && (!type || x.type === type));
-  if (!item) return sendJson(res, 404, { error: 'Meta no encontrada' });
-  return sendJson(res, 200, { meta: makeMeta(item, db) });
+ sendJson(res,200,{ok:true,type:"meta.js"})
 };
